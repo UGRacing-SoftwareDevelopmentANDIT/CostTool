effbbb352a1afd70eb4c54ce6d72c586b697c993
LT <lewistse2000@hotmail.com>
2021-11-10 16:53:01 +0000

    creating add_assembly template and configuring url Created the template for adding an addAssembly Configured URL so that it is accessible I do have the code for the view and form 95% ready, if required

--- tool/urls.py
+++ tool/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from tool import views
+
+
 app_name = 'tool'
 urlpatterns = [
-    
 path('', views.home, name='home'), 
 path('car/addCar', views.add_car, name = 'add_car'),
 path('car/<slug:car_slug>', views.car_display, name = 'car_display'),
 path('car/<slug:car_slug>/<slug:system_slug>', views.system_display, name = 'system_display'),
+path('car/<slug:car_slug>/<slug:system_slug>/add-assembly', views.add_assembly, name="add_assembly"),
 path('car/<slug:car_slug>/<slug:system_slug>/<slug:assembly_slug>', views.assembly_display, name = 'assembly_display'),
 path('car/<slug:car_slug>/<slug:system_slug>/<slug:assembly_slug>/<slug:part_slug>', views.part_display, name = 'part_display'),
 path('car/<slug:car_slug>/<slug:system_slug>/<slug:assembly_slug>/<slug:part_slug>/<slug:pmft_slug>', views.pmft_display, name = 'pmft_display'),

ba5631bde1c89bab460522b80f4d7369cfcffbf9
Drew Edgar <drewpedgar@gmail.com>
2022-01-19 16:25:35 +0000

    added user allocation models changed user verified attribute to rank

--- tool/models.py
+++ tool/models.py
@@ -17,11 +17,23 @@ verified which is a boolean value used to keep track whether a suer is verified 
 class UserAccount(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, unique=True, primary_key=True)
-    verified = models.BooleanField(default=False)
+    rank = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.username
 
+class Subteam(models.Model):
+    teamName = models.CharField(max_length=20, unique=True, primary_key=True)
+    abbr = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.teamName
+
+class TeamLinking(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(UserAccount, on_delete=models.CASCADE)
+    subteam = models.OneToOneField(Subteam, on_delete=models.CASCADE)
+    teamHead = models.BooleanField(default=False)
 
 class Car(models.Model):
     #this will be auto created by conjoining name and year
